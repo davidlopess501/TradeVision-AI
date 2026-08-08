d245fc9acd1d9cf8ae90627201fd7e3bf96c3eb9
davidlopess501 <cinn46579@gmail.com>
2026-08-08 21:56:47 +0000

    Aumenta historico do Backtesting para 5000 candles

--- src/screens/Analysis.tsx
+++ src/screens/Analysis.tsx
@@ -266,7 +266,7 @@ export default function AnalysisScreen({
           provider.getCandles(
             asset,
             timeframe,
-            1000,
+            5000,
           ),
         ]);
 
